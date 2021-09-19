c3be68659978e3c9c881e29aa2c626e9a95a097c
hansprayoga <hansprayogabusiness@gmail.com>
2021-09-19 19:27:46 +0700

    perubahan isi Contoh1.php

--- Contoh1.php
+++ Contoh1.php
@@ -1,1 +1,13 @@
-Contoh1.php
+<?php
+
+    class Contoh1 extends CI_Controller
+
+    {
+        public function index ()
+        {
+            echo "Perkenalkan";
+            echo "Nama Saya Hans Prayoga Bahtiar ...  Ini tugas WP2_Pert.1";
+        }
+    }
+
+?>
