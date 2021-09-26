36ede7a0575375bf7fdaee3b2addd67ab42e9687
hansprayoga <hansprayogabusiness@gmail.com>
2021-09-26 17:29:50 +0700

    add file model_latihan1.php, latihan1.php, view-latihan1.php

---
+++ view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>latihan1</title>
+    </head>
+
+    <body style="text-align: center;">
+        Hallo Kawan ... ini web programming<br>
+        Nilai 1 = <?= $nilai1; ?><br>
+        Nilai 2 = <?= $nilai2; ?><br>
+        Hasil menggunakan metode MVC yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+</body>
+</html>
