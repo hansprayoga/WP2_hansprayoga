07cad669688c5e7ae1ba3cdd25d3ba6e9d09904b
hansprayoga <hansprayogabusiness@gmail.com>
2021-10-03 10:56:46 +0700

    add file v_header.php

---
+++ v_header.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Web Program II || merancang template</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?> assets/css/stylebuku.css">
+</head>
+<body> 
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat tampilan sederhana dengan ci</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
+</body>
